4b33cafb96e62f985300c13078e0e0257c94cf6e
mikeaom <56056527+mikeaom@users.noreply.github.com>
2021-02-12 19:02:39 +0700

    ttt

--- engine/lib/db.php
+++ engine/lib/db.php
@@ -4,4 +4,4 @@ define("DB_HOST", "localhost");
 define("DB_PORT", "3306");
 define("DB_DATABASE", "suwan_main");
 define("DB_USERNAME", "root");
-define("DB_PASSWORD", "123456789");
+define("DB_PASSWORD", "");
---
+++ home/index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+	<title>Login</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="../images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="../vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="../vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../css/util.css">
+	<link rel="stylesheet" type="text/css" href="../css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form">
+					<span class="login100-form-title p-b-43">
+						Login to continue
+					</span>
+					
+					
+					<div class="wrap-input100 validate-input" data-validate = "กรุณากรอก Username">
+						<input class="input100" type="text" name="email">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Username</span>
+					</div>
+					
+		<style>
+		.show-password {
+		cursor: pointer; 
+		position: absolute;
+		margin-bottom: 26px; 
+		margin-left: 410px;
+		}
+		</style>			
+					<div class="wrap-input100 validate-input" data-validate="กรุณากรอก Password">
+						<input class="input100" type="password" name="password" id="txtPassword">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Password</span>
+						<div class="show-password">
+								<span id="a_pass" onclick="showPass()"><i class="fa fa-eye-slash"></i></span>
+							</div>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+				</form>
+
+				<div class="login100-more" style="background-image: url('../images/bg-01.jpg');">
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+<!--===============================================================================================-->
+	<script src="../vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/bootstrap/js/popper.js"></script>
+	<script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/daterangepicker/moment.min.js"></script>
+	<script src="../vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="../js/main.js"></script>
+
+</body>
+</html>
+<script>
+//show pass	
+function showPass() {
+  var x = document.getElementById("txtPassword");
+  if (x.type === "password") {
+    x.type = "text";
+	document.getElementById("a_pass").innerHTML = "<i class='fa fa-eye'></i>";  
+  } else {
+    x.type = "password";
+	document.getElementById("a_pass").innerHTML = "<i class='fa fa-eye-slash'></i>";  
+  }
+}
+</script>
