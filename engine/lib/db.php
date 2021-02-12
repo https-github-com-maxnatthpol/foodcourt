1ce7a37da803c4505fcc77d3e5dfd0a66a6d30e9
maxnatthpol <tike.natthpol@hotmail.com>
2021-02-12 17:21:10 +0700

    Update db.php
    conn db

--- engine/lib/db.php
+++ engine/lib/db.php
@@ -1,7 +1,7 @@
 <?php
 
-define("DB_HOST", "localhost");
+define("DB_HOST", "192.168.1.145");
 define("DB_PORT", "3306");
 define("DB_DATABASE", "suwan_main");
 define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "123456789");
