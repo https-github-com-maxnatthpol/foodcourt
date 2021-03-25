e7a4f19c345722568f61f6eb192e4608cc6837c9
maxnatthpol <tike.natthpol@hotmail.com>
2021-03-26 03:17:39 +0700

    5

--- engine/mod_card/js/javascript.js
+++ engine/mod_card/js/javascript.js
@@ -1,4 +1,5 @@
 fetch_data_table();
+
 function fetch_data_table() {
   var button_update = $("#per_button_edit").val();
   var button_delete = $("#per_button_del").val();
@@ -14,97 +15,121 @@ function fetch_data_table() {
       button_create: button_create,
       button_view: button_view
     },
-    success: function(data) {
+    success: function (data) {
       $("#div_table_list").html(data);
       $("#table_front_manage").DataTable({
         scrollY: true,
         scrollCollapse: true,
         scrollX: true,
-        order: [[1, "asc"]],
-		  		language: {
-                sEmptyTable: "ไม่มีข้อมูลในตาราง",
-				sInfo: "แสดง _START_ ถึง _END_ จาก _TOTAL_ แถว",
-				sInfoEmpty: "แสดง 0 ถึง 0 จาก 0 แถว",
-				sInfoFiltered: "(กรองข้อมูล _MAX_ ทุกแถว)",
-				sInfoPostFix: "",
-				sInfoThousands: ",",
-				sLengthMenu: "แสดง _MENU_ แถว",
-				sLoadingRecords: "กำลังโหลดข้อมูล...",
-				sProcessing: "กำลังดำเนินการ...",
-				sSearch: "ค้นหา: ",
-				sZeroRecords: "ไม่พบข้อมูล",
-				oPaginate: {
-				sFirst: "หน้าแรก",
-				sPrevious: "ก่อนหน้า",
-				sNext: "ถัดไป",
-				sLast: "หน้าสุดท้าย",	
-				},
-				oAria: {
-				sSortAscending: ": เปิดใช้งานการเรียงข้อมูลจากน้อยไปมาก",
-				sSortDescending: ": เปิดใช้งานการเรียงข้อมูลจากมากไปน้อย",
-				},
-            }
+        order: [
+          [1, "asc"]
+        ],
+        language: {
+          sEmptyTable: "ไม่มีข้อมูลในตาราง",
+          sInfo: "แสดง _START_ ถึง _END_ จาก _TOTAL_ แถว",
+          sInfoEmpty: "แสดง 0 ถึง 0 จาก 0 แถว",
+          sInfoFiltered: "(กรองข้อมูล _MAX_ ทุกแถว)",
+          sInfoPostFix: "",
+          sInfoThousands: ",",
+          sLengthMenu: "แสดง _MENU_ แถว",
+          sLoadingRecords: "กำลังโหลดข้อมูล...",
+          sProcessing: "กำลังดำเนินการ...",
+          sSearch: "ค้นหา: ",
+          sZeroRecords: "ไม่พบข้อมูล",
+          oPaginate: {
+            sFirst: "หน้าแรก",
+            sPrevious: "ก่อนหน้า",
+            sNext: "ถัดไป",
+            sLast: "หน้าสุดท้าย",
+          },
+          oAria: {
+            sSortAscending: ": เปิดใช้งานการเรียงข้อมูลจากน้อยไปมาก",
+            sSortDescending: ": เปิดใช้งานการเรียงข้อมูลจากมากไปน้อย",
+          },
+        }
       });
     }
   });
 }
 
 $(document).on('click', '#add_btn', function () {
-    var formData = new FormData($('#frm_select_num')[0]);
-    $.ajax({
-      method: "POST",
-      url: "functions.php",
-      data: formData,
-      cache: false,
-      contentType: false,
-      processData: false,
-      success: function (response) {
-        if (response.status == '1') {;
-          document.getElementById("number").value  = response.message;
-        }
-      },
-    });
-  
+  var formData = new FormData($('#frm_select_num')[0]);
+  $.ajax({
+    method: "POST",
+    url: "functions.php",
+    data: formData,
+    cache: false,
+    contentType: false,
+    processData: false,
+    success: function (response) {
+      if (response.status == '1') {
+        ;
+        document.getElementById("number").value = response.message;
+      }
+    },
+  });
+
 });
 
 $(document).on('click', '#btnSendAdd', function () {
-  var formData = new FormData($('#form_add')[0]);
-  swal.fire({
-    title: "ตรวจสอบข้อมูลก่อนยืนยัน !",
-    icon: 'info',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    cancelButtonText: 'ยกเลิก!',
-    confirmButtonText: 'ยืนยัน',
-    reverseButtons: true
+  if (document.getElementById("card_number_1").value == "") {
+    swal.fire("เกิดปัญหากับระบบ", "กรุณากรอกรหัสบัตร", "warning");
+  } else {
+    var formData = new FormData($('#form_add')[0]);
+    swal.fire({
+      title: "ตรวจสอบข้อมูลก่อนยืนยัน !",
+      icon: 'info',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'ยกเลิก!',
+      confirmButtonText: 'ยืนยัน',
+      reverseButtons: true
 
-  }).then((result) => {
-    if (result.value) {
-      $.ajax({
-        method: "POST",
-        url: "functions.php",
-        data: formData,
-        cache: false,
-        contentType: false,
-        processData: false,
-        success: function (data) {
-          if (data.status == '0') {
-            swal.fire("สำเร็จ", "บันทึกเรียบร้อยแล้ว", "success");
-            document.getElementById("form_add").reset();
-            setTimeout(function () {
-              location.reload();
-            }, 1000);
+    }).then((result) => {
+      if (result.value) {
+        $.ajax({
+          method: "POST",
+          url: "functions.php",
+          data: formData,
+          cache: false,
+          contentType: false,
+          processData: false,
+          success: function (data) {
+            if (data.status == '0') {
+              swal.fire("สำเร็จ", "บันทึกเรียบร้อยแล้ว", "success");
+              document.getElementById("form_add").reset();
+              setTimeout(function () {
+                location.reload();
+              }, 1000);
 
-          } else if (data.status == 2) {
-            swal.fire("ไม่สำเร็จ", "มีรหัสบัตรนี้แล้ว", "warning");
-          } else {
-            swal.fire("ไม่สำเร็จ", "เกิดปัญหากับระบบ", "warning");
-          }
-        },
-      }).fail(function () {
-        swal.fire("ไม่สำเร็จ", "เกิดปัญหากับระบบ", "warning");
-      });
+            } else if (data.status == 2) {
+              swal.fire("ไม่สำเร็จ", "มีรหัสบัตรนี้แล้ว", "warning");
+            } else {
+              swal.fire("ไม่สำเร็จ", "เกิดปัญหากับระบบ", "warning");
+            }
+          },
+        }).fail(function () {
+          swal.fire("ไม่สำเร็จ", "เกิดปัญหากับระบบ", "warning");
+        });
+      }
+    });
+  }
+
+});
+
+$(document).on("click", ".edit_btn", function() {
+  form = "select_div_edit_front_manage";
+  id = $(this).attr("data-id");
+  $.ajax({
+    url: "select_data.php",
+    method: "POST",
+    data: {
+      form: form,
+      id: id
+    },
+    success: function(data) {
+      $("#div_edit").html(data);
     }
   });
 });
--- engine/mod_card/select_data.php
+++ engine/mod_card/select_data.php
@@ -12,6 +12,8 @@ if (isset($_POST['form'])) {
     select_table_front_manage();
   } elseif ($_POST['form'] == "frm_select_num") {
     frm_select_num();
+  } else if ($_POST['form'] == "select_div_edit_front_manage") {
+    select_div_edit_front_manage();
   }
 }
 
@@ -43,7 +45,7 @@ function select_table_front_manage()
       ?>
         <tr class="show-tr">
           <td>
-            <?php echo  $objResult["number"]; ?>
+            <?php echo  str_pad($objResult["number"], 4, "0", STR_PAD_LEFT); ?>
           </td>
           <td>
             <?php echo $objResult["card_number"]; ?>
@@ -55,9 +57,7 @@ function select_table_front_manage()
             <?php echo  $objResult["amount"]; ?>
           </td>
           <td>
-            <a data-toggle="modal" data-target="#modal_edit" style="<?php echo $button_update ?>" class="btn btn-warning  btn-sm edit_btn" data-id="<?php echo $objResult['id_cashier'] ?>"><i class="fa fa-edit" style="color: white;"></i></a>
-
-            <button type="button" class="btn btn-danger  btn-sm delete_btn" style="<?php echo $button_delete ?>" data-id="<?php echo $objResult['id_cashier'] ?>"><i class="mdi mdi-delete-empty" style="color: white;"></i></button>
+            <a data-toggle="modal" data-target="#modal_edit" style="<?php echo $button_update ?>" class="btn btn-warning  btn-sm edit_btn" data-id="<?php echo $objResult['id'] ?>"><i class="fa fa-edit" style="color: white;"></i></a>
           </td>
         </tr>
       <?php
@@ -67,5 +67,134 @@ function select_table_front_manage()
     </tbody>
   </table>
   <input type="hidden" name="hdnCount" value="<?php echo $num ?>">
+<?php
+}
+
+function select_div_edit_front_manage()
+{
+  $db = new DB();
+
+  $strSQL = "SELECT * ,IF(status = 0, 'ระงับการใช้งาน', 'ใช้งานได้') AS text_status FROM `card`
+             WHERE id = '" . $_POST["id"] . "';
+             ";
+  $objQuery = $db->Query($strSQL);
+  $objResult = mysqli_fetch_array($objQuery, MYSQLI_ASSOC);
+
+?>
+
+  <div class="card card-body">
+    <div class="box-header with-border">
+      <h3 class="box-title">แก้ไขข้อมูลบัตร</h3>
+
+    </div>
+    <div class="box-body" style="padding: 0;">
+      <form action="" name="form_edit" id="form_edit" method="post">
+        <input type="hidden" name="form" value="form_edit">
+        <input type="hidden" name="id_edit" value="<?php echo $_POST["id"] ?>">
+        <div id="div_form_edit">
+          <div class="row">
+
+            <div class="col-md-3">
+
+              <div class="form-group">
+                <label for="example-email">หมายเลขบัตร </label>
+                <input type="text" class="form-control" id="number" name="number" placeholder="กรอกหมายเลขบัตร" value="<?=$objResult['number'];?>">
+              </div>
+
+            </div>
+
+            <div class="col-md-9">
+
+              <div class="form-group">
+                <label for="example-email">รหัสบัตร </label>
+                <input type="text" class="form-control" id="card_number_1" name="card_number" placeholder="กรอกรหัสบัตร" value="<?=$objResult['card_number'];?>">
+              </div>
+
+            </div>
+
+            <div class="col-md-3">
+
+              <div class="form-group">
+                <label for="example-email">สถานะบัตร </label>
+                <select class="form-control">
+                <option>1</option>
+                <option>2</option>
+                </select>
+              </div>
+
+            </div>
+
+            <div class="col-md-3">
+
+              <div class="form-group">
+                <label for="example-email">จำนวนเงินในบัตร </label>
+                <input type="text" class="form-control" id="card_number_1" name="card_number"  value="<?=$objResult['amount'];?>" disabled>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+    </div>
+
+    <div class="boxsave" id="box-del-list" align="center">
+
+      <button type="button" class="btn btn-danger" data-dismiss="modal">ปิด</button>
+
+      <button type="button" class="btn btn-success  btnSendAdd" id="btnSendEdit" style="transition: 0.4s; margin-left: 5px;"><i class="fa fa-floppy-o" aria-hidden="true"></i>&nbsp;บันทึก</button>
+
+    </div>
+
+  </div>
+
+  </form>
+  </div>
+  </div>
+  </div>
+  <script type="text/javascript">
+    $(".dropify").dropify({
+      messages: {
+        //          default: "Drag and drop a file here or click",
+        default: "<span style='font-size: 16px; font-family: Sarabun, sans-serif;'>ลากและวางไฟล์ที่นี่หรือคลิก</span>",
+        //          replace: "Drag and drop or click to replace",
+        replace: "<span style='font-size: 14px; font-family: Sarabun, sans-serif;'>ลากและวางหรือคลิกเพื่อแทนที่</span>",
+        //          remove: "Remove",
+        remove: "<span style='font-size: 13px; font-family: Sarabun, sans-serif;'>ลบออก</span>",
+        error: "อ๊ะมีบางอย่างผิดปกติเกิดขึ้น"
+        //		  error: "Ooops, something wrong happended."	
+      },
+      tpl: {
+        message: '<div class="dropify-message" ><span class="file-icon" /> <p style="text-align: center;">{{ default }}</p></div>'
+      }
+    });
+
+    // Used events
+    var drEvent = $('#input-file-events').dropify();
+
+    drEvent.on('dropify.beforeClear', function(event, element) {
+      return confirm("Do you really want to delete \"" + element.file.name + "\" ?");
+    });
+
+    drEvent.on('dropify.afterClear', function(event, element) {
+      alert('File deleted');
+    });
+
+    drEvent.on('dropify.errors', function(event, element) {
+      console.log('Has Errors');
+    });
+
+    var drDestroy = $('#input-file-to-destroy').dropify();
+    drDestroy = drDestroy.data('dropify')
+    $('#toggleDropify').on('click', function(e) {
+      e.preventDefault();
+      if (drDestroy.isDropified()) {
+        drDestroy.destroy();
+      } else {
+        drDestroy.init();
+      }
+    })
+
+  </script>
 <?php
 }
